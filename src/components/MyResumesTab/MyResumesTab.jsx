b3676e1c13589905f1322b8693428946b1004f90
Mohammed Kaif <shaikmohammedkaif98@gmail.com>
2025-07-19 12:53:25 +0530

    fixing ui 67

--- src/components/MyResumesTab/MyResumesTab.jsx
+++ src/components/MyResumesTab/MyResumesTab.jsx
@@ -1,4 +1,4 @@
-import { useContext, useState } from 'react';
+import { useContext, useEffect, useState } from 'react';
 import './MyResumesTab.css';
 import AppContext from '../../context/AppContext';
 import Resume from '../../pages/Resume';
